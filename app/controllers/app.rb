2cb44f54181b35a3221b6c82e09d2ffd7a2a6c07
Rachel Warbelow <rachelwarbelow@gmail.com>
2015-10-21 21:49:38 -0600

    finished

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -1,4 +1,10 @@
 class PartialArts < Sinatra::Base
+  helpers do
+    def artists_url
+      "<a href='/artists'>All Artists</a>"
+    end
+  end
+
   get '/' do
     @belt_statistics     = Artist.group("belt").count
     @category_statistics = Artist.group("category").count
