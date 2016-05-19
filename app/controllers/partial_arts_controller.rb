3c29e103863f70f78d4e60b66d44511be7420bfd
Andrew Carmer <carmerandrew@gmail.com>
2016-05-19 11:21:31 -0600

    partials, helpers implemented

--- app/controllers/partial_arts_controller.rb
+++ app/controllers/partial_arts_controller.rb
@@ -1,4 +1,19 @@
 class PartialArts < Sinatra::Base
+  helpers do
+    def artists_url
+      "<a href='/artists'>All Artists</a>"
+    end
+
+    def root_url
+      "<a href='/'>Home</a>"
+    end
+
+    def link_to(name_of_link, path, class_name=nil)
+      "<a href='#{path}' class='#{class_name}'>#{name_of_link}</a>"
+    end
+  end
+
+
   get '/' do
     @belt_statistics     = Artist.group("belt").count
     @category_statistics = Artist.group("category").count
